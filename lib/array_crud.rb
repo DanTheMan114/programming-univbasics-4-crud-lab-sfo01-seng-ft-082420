56b2d30cd8010c6e6f3d7f27ab76e13b31508a41
Daniel Turner <danielturner184@gmail.com>
2020-08-07 05:23:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -45,7 +45,7 @@ def retrieve_last_element_from_array(array)
   
 end
 
-def update_element_from_index(array, index_number, element)
-array[]
+def update_element_from_index(array, 4, totally)
+array[3]
 
 end
